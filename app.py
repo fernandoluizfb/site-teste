28facf904a2b429f7dca466350e34bf74ac62c43
fernandoluizfb <99805892+fernandoluizfb@users.noreply.github.com>
2023-03-13 22:26:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+!pip install tchan
+
 from flask import Flask
+from tchan import ChannelScraper
 
 app = Flask(__name__)
 
@@ -18,3 +21,15 @@ def sobre():
 @app.route("/contato")
 def contato():
   return menu + "Aqui vai o conteúdo da página contato"
+
+@app.route("/promocoes")
+def ultimas_promocoes():
+  scraper = ChannelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return resultado
