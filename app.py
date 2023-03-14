ddc1a3030c5c55ddfffa7d0bd52d793b4f3b8ce4
fernandoluizfb <99805892+fernandoluizfb@users.noreply.github.com>
2023-03-13 22:09:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+"""
+<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<br>
+"""
+
 @app.route("/")
 def hello_world():
-  return "Olá, mundo! Esse é meu site. (Fernando Barbosa)"
+  return menu + "Olá, mundo! Esse é meu site. (Fernando Barbosa)"
 
 @app.route("/sobre")
 def sobre():
-  return "Aqui vai o conteúdo da página sobre"
+  return menu + "Aqui vai o conteúdo da página sobre"
 
 @app.route("/contato")
 def contato():
-  return "Aqui vai o conteúdo da página contato"
+  return menu + "Aqui vai o conteúdo da página contato"
