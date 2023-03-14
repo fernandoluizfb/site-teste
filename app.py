f5958a1d278e6abc0f17d37272e062a6ef42ac2c
fernandoluizfb <99805892+fernandoluizfb@users.noreply.github.com>
2023-03-13 21:50:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Esse é meu site. (Fernando Barbosa)"
+
+@app.route("/sobre")
+def sobre():
+  return "Aqui vai o conteúdo da página sobre"
+
+@app.route("/contato")
+def contato():
+  return "Aqui vai o conteúdo da página contato"
