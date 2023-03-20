040f4e0b685a1f5f15dd0a5cf2114d13b54d2eeb
fernandoluizfb <99805892+fernandoluizfb@users.noreply.github.com>
2023-03-20 20:52:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ from tchan import ChannelScraper
 
 app = Flask(__name__)
 
-"""
+def ultimas_promocoes():
+  scraper = ChannelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return resultado
+
+menu = """
 <a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> | <a href="/promocoes">Promoções</a>
 <br>
 """
